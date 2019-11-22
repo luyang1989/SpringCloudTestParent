69c7ebfd9b6a9640a44f919bab2d912d2975bbbd
luyang <654922729@qq.com>
2019-11-22 16:45:56 +0800

    修改

--- zuul/src/main/java/com/example/zuul/service/LoginService.java
+++ zuul/src/main/java/com/example/zuul/service/LoginService.java
@@ -1,11 +1,7 @@
 package com.example.zuul.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.base.sys.config.NumConstant;
 import com.example.base.sys.entity.SecUser;
-import com.sun.org.apache.xpath.internal.operations.Bool;
-import org.apache.commons.lang.StringUtils;
-import org.springframework.stereotype.Service;
 
 public interface LoginService extends IService<SecUser> {
 
