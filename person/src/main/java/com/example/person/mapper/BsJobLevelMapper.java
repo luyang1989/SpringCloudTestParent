5e9cde93cc5612401138ece07210cbdff5e2bf44
a2602843590 <huangyufeiupup@163.com>
2019-12-10 14:14:24 +0800

    职级管理 练习 V1.0

---
+++ person/src/main/java/com/example/person/mapper/BsJobLevelMapper.java
@@ -0,0 +1,16 @@
+package com.example.person.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.base.sys.entity.BsJobLevel;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author hyf
+ * @since 2019-12-10
+ */
+public interface BsJobLevelMapper extends BaseMapper<BsJobLevel> {
+
+}
